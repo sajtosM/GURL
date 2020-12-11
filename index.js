81c05e7e40bea5d57a5beba33e69ed51d3b71cb5
sajtosM <sajtos.matyas@gmail.com>
2020-12-12 00:26:21 +0100

    🔨 Added --init for initialization

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
+const readline = require('readline');
+const path = require('path');
+const fs = require('fs');
+
 const { RssFeedHandler } = require('./RssFeedHandler');
 
-if (process.argv[2]) {
+if (process.argv.some((argv) => argv === '--init')) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.question('Email address do you want to send the mails? ', function (sToEmail) {
+        rl.question('Email address do you want to use to send the mails? ', function (sFromEmail) {
+            rl.question('Password for the Email address? ', function (sPass) {
+                const configFile = {
+                    target: sToEmail,
+                    auth: {
+                        user: sFromEmail,
+                        pass: sPass
+                    }
+                };
+                console.log(configFile);
+                rl.question('Do you want to save the config?(y/n) ', function (sYesNo) {
+                    if (sYesNo.toLocaleLowerCase() === 'y') {
+                        fs.writeFileSync( path.join(__dirname,'config.json'), JSON.stringify(configFile));
+                        process.exit(0);
+                    }
+                });
+            });
+        });
+    });
+
+} else if (process.argv[2]) {
     let URL = process.argv[2];
-    let bSendMail = process.argv.some((argv) => argv === '--sendMail');
+    let bSendMail = !process.argv.some((argv) => argv === '--noMail');
     let nLimit;
     if (!isNaN(process.argv[3])) {
         nLimit = process.argv[3];
